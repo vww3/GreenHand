50b3ab03c47ae83df0cec39410b7304a6d1856e9
Mickaël Boidin <mickael.boidin@icloud.com>
2017-01-09 14:52:39 +0100

    suppression de l'excedent

--- view/Test/index.php
+++
@@ -1,3 +0,0 @@
-<?php foreach ($test as $t) : ?>
-	<h1><a href="<?php echo WEBROOT; ?>Test/view/<?php echo $t->id; ?>"><?php echo $t->name; ?></a></h1>
-<?php endforeach; ?>
--- view/layout/default.php
+++
@@ -1,7 +0,0 @@
-<h1>Salut, bienvneue sur mon site</h1>
-
-
-<?php echo $content_for_layout; ?>
-
-
-<p>Ici le footer</p>
