a83939633539270bfd0d9b3bf3312a27bb4436a0
vww3 <emma.louviot@gmail.com>
2017-01-12 15:52:59 +0100

    Ajout du header de la page profil

---
+++ View/monprofil/index.php
@@ -0,0 +1,31 @@
+<header>
+	<div class="logo_greenhand">
+		<h1><img src="<?= IMAGE ;?>/accueil/logo_blanc.svg" alt="Logo GreenHand"></h1>
+	</div>
+
+	<div class="main_menu">
+		<nav>
+			<a href="#">Les défis</a>
+			<a href="#">Les défis</a>
+			<a href="#">Les défis</a>
+			<a href="#">Les défis</a>
+		</nav>
+	</div>
+
+	<div class="search">
+		<input type="search" name="main_search" placeholder="rechercher du vert...">
+	</div>
+
+	<div class="notif_menu">
+		<div class="notif_top" id="notif_alert">
+			<img src="<?= IMAGE ;?>/accueil/notif_alert_blanc.svg" alt="notif GreenHand">
+		</div>
+		<div class="notif_top" id="notif_msg">
+			<img src="<?= IMAGE ;?>/accueil/notif_msg_blanc.svg" alt="message GreenHand">
+		</div>
+		<div class="notif_top" id="notif_profil">
+			<img src="<?= IMAGE ;?>/accueil/notif_profil_blanc.svg" alt="profil GreenHand">
+		</div>
+	</div>
+
+</header>
