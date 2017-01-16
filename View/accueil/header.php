15f8424fdbf54f3e1b18f7561f71e3ce3aaf317b
vww3 <emma.louviot@gmail.com>
2017-01-16 16:58:40 +0100

    Amélioration du css du menu principal + transformation des images svg en typo

--- View/accueil/header.php
+++ View/accueil/header.php
@@ -2,22 +2,22 @@
 	<header>
 		<h1><a href="<?= BASE ?>accueil"><img src="<?= IMAGE ;?>/accueil/logo.svg" alt="Logo GreenHand"></a></h1>
 
+		<div class="baseline">
+			<h2>Défiez vos amis !</h2>
+		</div>
+
 		<div class="header_notif">
 			<div class="header_notif_top">
-				<div class="notif_top" id="notif_alert">
-					<img src="<?= IMAGE ;?>/accueil/notif_alert.svg" alt="notif GreenHand">
-				</div>
-				<div class="notif_top" id="notif_msg">
-					<img src="<?= IMAGE ;?>/accueil/notif_msg.svg" alt="message GreenHand">
-				</div>
-				<div class="notif_top" id="notif_profil">
-					<img src="<?= IMAGE ;?>/accueil/notif_profil.svg" alt="profil GreenHand">
-				</div>
+				<div class="notif_top" id="notif_alert"></div>
+				<div class="notif_top" id="notif_msg"></div>
+				<div class="notif_top" id="notif_profil"></div>
 			</div>
 
 			<input type="search" name="main_search" class="header_notif_search" placeholder="rechercher du vert...">
 		</div>
 	</header>
+
+
 
 	<?php include('menu.php'); ?>
 
--- View/accueil/menu.php
+++ View/accueil/menu.php
@@ -1,31 +1,24 @@
 <div class="main_menu">
 	<nav>
 		<a href="#" class="item_menu">
-			<div class="content_img_menu">
-				<img src="<?= IMAGE ;?>/accueil/menu_1.svg">
+			<div class="content_img_menu" id="leaves">
 			</div>
 			<span>Les défis</span>
 		</a>
 
 		<a href="#" class="item_menu">
-			<div class="content_img_menu">
-				<img src="<?= IMAGE ;?>/accueil/menu_2.svg">
-			</div>
-			<span>Les défis</span>
+			<div class="content_img_menu" id="internet"></div>
+			<span>Recycler</span>
 		</a>
 
 		<a href="#" class="item_menu">
-			<div class="content_img_menu">
-				<img src="<?= IMAGE ;?>/accueil/menu_3.svg">
-			</div>
-			<span>Les défis</span>
+			<div class="content_img_menu" id="salad"></div>
+			<span>Producteurs</span>
 		</a>
 
 		<a href="#" class="item_menu">
-			<div class="content_img_menu">
-				<img src="<?= IMAGE ;?>/accueil/menu_4.svg">
-			</div>
-			<span>Les défis</span>
+			<div class="content_img_menu" id="water"></div>
+			<span>Cartes</span>
 		</a>
 
 
