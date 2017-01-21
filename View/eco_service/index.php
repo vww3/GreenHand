a33abe400fc06bd1483590f523ee7f882ed7aae6
Mickaël Boidin <mickael.boidin@icloud.com>
2017-01-21 18:46:36 +0100

    Design de eco-service

--- View/eco_service/index.php
+++ View/eco_service/index.php
@@ -4,7 +4,7 @@ include('header.php');
 ?>
 
 <div class="main_container" id="eco_service">
-	<h2>Les ressources Eco-responsable</h2>
+	<h2>Annuaire des services éco-responsables</h2>
 	<nav>
 	<?php foreach($categories as $category) { ?>
 		<a href="#<?= $category->idAttr ?><?= $category->id ?>" id="<?= $category->idAttr ?>"><?= $category->title ?></a>
@@ -14,29 +14,42 @@ include('header.php');
 	<p id="new_service">Proposez un nouveau service ? Pas de problème</p>
 
 	<form method="post" id="form_service" enctype="multipart/form-data">
-		<?= $formNewService->selection('category', $categoriesSelection) ?>
-		<?= $formNewService->text('title', ['placeholder' => 'Titre du service']) ?>
-		<?= $formNewService->text('link', ['placeholder' => 'Lien']) ?>
-		<?= $formNewService->textarea('description', ['placeholder' => 'Description']) ?>
-		<?= $formNewService->file('logo') ?>
-		<?= $formNewService->sender('Envoyer la proposition') ?>
+		<section>
+			<div class="half">
+			<?= $formNewService->text('title', ['placeholder' => 'Titre du service']) ?>
+			<?= $formNewService->text('link', ['placeholder' => 'Lien vers le service : http://................']) ?>
+			<?= $formNewService->selection('category', $categoriesSelection) ?>
+			<?= $formNewService->file('logo') ?>
+			</div>
+			<div class="half">
+			<?= $formNewService->textarea('description', ['placeholder' => 'Une courte description du service que vous proposez']) ?>
+			<?= $formNewService->sender('Envoyer la proposition') ?>
+			</div>
+		</section>
 	</form>
 		
 	<?php foreach($services as $id => $category) { ?>
+	<article>
 		<h3 id="<?= $category['id'] ?><?= $id ?>"><?= $category['title'] ?></h3>
 		
 		<div class="content_article">
 		<?php foreach($category['services'] as $service) { ?>
-			<a target="_blank" href="<?= $service->link ?>" class="col3">
-				<img src="<?= IMAGE ?>eco_service/<?= $service->id ?>.jpg" alt="Logo GreenHand">
-				<h4><?= $service->title ?></h4>
-				<p><?= $service->description ?></p>
-				<!--<div class="like">
-					<span>2</span>
-				</div>-->
-			</a>
+			<div class="service">
+				<a class="card" target="_blank" href="<?= $service->link ?>">
+					<div>
+						<h4><?= $service->title ?></h4>
+						<p><?= $service->description ?></p>
+						<!--<div class="like">
+							<span>2</span>
+						</div>-->
+						<p class="link"><b>Lien : </b><?= $service->link ?></p>
+					</div>
+					<img src="<?= IMAGE ?>eco_service/<?= $service->id ?>.jpg" alt="Logo GreenHand">
+				</a>
+			</div>
 		<?php } ?>
 		</div>
+	</article>
 	<?php } ?>
 </div>
 
